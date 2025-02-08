eddfed2c390c77014cf6b2a4cef04d21e4104411
guney <guneyyilmaz2707@gmail.com>
2025-02-08 13:38:17 +0300

    only one coin flip can be active in a channel at a time

--- src/main/kotlin/command/commands/CoinFlipCommand.kt
+++ src/main/kotlin/command/commands/CoinFlipCommand.kt
@@ -10,13 +10,28 @@ import kotlin.random.Random
 
 class CoinFlipCommand(name: String, description: String) : ICommand(name, description) {
 
+    companion object {
+        private val activeChannels = mutableSetOf<Long>()
+    }
+
     @OptIn(DelicateCoroutinesApi::class)
     override fun execute(member: Member, message: Message, args: Array<String>) {
+        val channelId = message.channel.idLong
+
+        if (activeChannels.contains(channelId)) {
+            message.reply("A coin flip is already in progress in this channel. Please wait.").queue()
+            return
+        }
+
+        activeChannels.add(channelId)
+
         message.channel.sendMessage("<a:coinflip:1233732940949885039> Flipping...").queue { sentMessage ->
             GlobalScope.launch {
                 delay(2000)
                 val result = if (Random.nextBoolean()) "<:head:1233732880027615273> Heads!" else "<:tail:1233733238992801813> Tails!"
-                sentMessage.editMessage(":coin: Coinflip result: $result").queue()
+                sentMessage.editMessage(":coin: Coinflip result: $result").queue {
+                    activeChannels.remove(channelId)
+                }
             }
         }
     }
